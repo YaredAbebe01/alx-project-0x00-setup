727cb225626c087964659464ce2ef28a62a6557d
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2026-02-17 10:29:11 +0300

    Refactor Button component to use styles prop

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,13 +1,11 @@
 import { ButtonProps } from "@/interfaces"
 
-const Button: React.FC<ButtonProps> = ({ title, className }) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   return (
-    <button
-      className={`px-4 py-2 bg-blue-500 text-white ${className}`}
-    >
+    <button className={styles}>
       {title}
     </button>
   )
 }
 
-export default Button
+export default Button;
